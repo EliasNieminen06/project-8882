920118441007a097ba34cf9e35ee600c98ed9273
EliasNieminen06 <elias.nieminen@edu.hel.fi>
2024-08-21 15:07:51 +0300

    save

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,10 +10,16 @@ public class GameManager : MonoBehaviour
     public int currentCheckpoint = 0;
     public GameObject pauseMenu;
     public bool isPuased;
+    public GameObject player;
 
     private void Awake()
     {
         instance = this;
+    }
+
+    private void Start()
+    {
+        player = GameObject.Find("Player");
     }
 
     private void Update()
@@ -26,6 +32,18 @@ public class GameManager : MonoBehaviour
 
     void TogglePause()
     {
-
+        isPuased = !isPuased;
+        if (isPuased)
+        {
+            Time.timeScale = 0;
+            pauseMenu.SetActive(true);
+            player.GetComponent<Movement>().enabled = false;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pauseMenu.SetActive(false);
+            player.GetComponent<Movement>().enabled = true;
+        }
     }
 }
